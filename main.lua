a473f7d93fd423f844e7555ace4ae5c20349287a
seek lol.lua <162717999+Drop56796@users.noreply.github.com>
2024-06-10 21:06:32 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -388,6 +388,147 @@ coroutine.wrap(YUDP_fake_script)()
 local function YXITCY_fake_script() -- MainFrame.Drag script 
 	local script = Instance.new('Folder', MainFrame)
 
+local function NQBLJK_fake_script() -- MainFrame.LocalScript 
+	local script = Instance.new('LocalScript', MainFrame)
+
+	local frame = script.Parent.Parent.MainFrame --in the .frame - you should need to change it what is your frame name.
+	local hotkey = Enum.KeyCode.Insert --Change your keybind you want.
+	
+	local UIS = game:GetService("UserInputService")
+	local open = false
+	
+	
+	
+	UIS.InputBegan:Connect(function(key, gp)
+		if key.KeyCode == hotkey then
+			if UIS:GetFocusedTextBox() == nil then
+				if open == false then
+					open = true 
+					frame.Visible = open
+				elseif open == true then
+					open = false
+					frame.Visible = open
+				end
+			end
+		end
+	end)
+end
+coroutine.wrap(NQBLJK_fake_script)()
+
+game:GetService("StarterGui"):SetCore("SendNotification",{
+Title = "Press Insert to open/close",
+Text = "'", 
+
+Button1 = "",
+Button2 = "",
+Duration = 5 
+})
+
+local function SYHNK_fake_script() -- ImageLabel.LocalScript 
+    local script = Instance.new('LocalScript', ImageLabel)
+    local lua_keywords = {"and", "break", "do", "else", "elseif", "end", "false", "for", "function", "goto", "if", "in", "local", "nil", "not", "or", "repeat", "return", "then", "true", "until", "while"}
+    local global_env = {"getrawmetatable", "game", "workspace", "script", "math", "string", "table", "print", "wait", "BrickColor", "Color3", "next", "pairs", "ipairs", "select", "unpack", "Instance", "Vector2", "Vector3", "CFrame", "Ray", "UDim2", "Enum", "assert", "error", "warn", "tick", "loadstring", "_G", "shared", "getfenv", "setfenv", "newproxy", "setmetatable", "getmetatable", "os", "debug", "pcall", "ypcall", "xpcall", "rawequal", "rawset", "rawget", "tonumber", "tostring", "type", "typeof", "_VERSION", "coroutine", "delay", "require", "spawn", "LoadLibrary", "settings", "stats", "time", "UserSettings", "version", "Axes", "ColorSequence", "Faces", "ColorSequenceKeypoint", "NumberRange", "NumberSequence", "NumberSequenceKeypoint", "gcinfo", "elapsedTime", "collectgarbage", "PhysicalProperties", "Rect", "Region3", "Region3int16", "UDim", "Vector2int16", "Vector3int16"}
+    local Source = script.Parent.EditorFrame.Source
+    local scrollframe = script.Parent.EditorFrame
+    local Lines = Source.Parent.Lines
+    local Highlight = function(string, keywords)
+        local K = {}
+        local S = string
+        local Token =
+            {
+                ["="] = true,
+                ["."] = true,
+                [","] = true,
+                ["("] = true,
+                [")"] = true,
+                ["["] = true,
+                ["]"] = true,
+                ["{"] = true,
+                ["}"] = true,
+                [":"] = true,
+                ["*"] = true,
+                ["/"] = true,
+                ["+"] = true,
+                ["-"] = true,
+                ["%"] = true,
+                [";"] = true,
+                ["~"] = true
+            }
+        for i, v in pairs(keywords) do
+            K[v] = true
+        end
+        S = S:gsub(".", function(c)
+            if Token[c] ~= nil then
+                return "\32"
+            else
+                return c
+            end
+        end)
+        S = S:gsub("%S+", function(c)
+            if K[c] ~= nil then
+                return c
+            else
+                return (" "):rep(#c)
+            end
+        end)
+        return S
+    end
+    local hTokens = function(string)
+        local Token =
+            {
+                ["="] = true,
+                ["."] = true,
+                [","] = true,
+                ["("] = true,
+                [")"] = true,
+                ["["] = true,
+                ["]"] = true,
+                ["{"] = true,
+                ["}"] = true,
+                [":"] = true,
+                ["*"] = true,
+                ["/"] = true,
+                ["+"] = true,
+                ["-"] = true,
+                ["%"] = true,
+                [";"] = true,
+                ["~"] = true
+            }
+        local A = ""
+        string:gsub(".", function(c)
+            if Token[c] ~= nil then
+                A = A .. c
+            elseif c == "\n" then
+                A = A .. "\n"
+            elseif c == "\t" then
+                A = A .. "\t"
+            else
+                A = A .. "\32"
+            end
+        end)
+        return A
+    end
+
+    local strings = function(string)
+        local highlight = ""
+        local quote = false
+        string:gsub(".", function(c)
+            if quote == false and c == "\"" then
+                quote = true
+            elseif quote == true and c == "\"" then
+                quote = false
+            end
+            if quote == false and c == "\"" then
+                highlight = highlight .. "\""
+            elseif c == "\n" then
+                highlight = highlight .. "\n"
+            elseif c == "\t" then
+                highlight = highlight .. "\t"
+            elseif quote == true then
+                highlight = highlight .. c
+            elseif quote == false then
+                highlight = highlight .. "\32"
+
 	--Credits go to whoever made this script.
 	
 	local UIS = game:GetService("UserInputService")
